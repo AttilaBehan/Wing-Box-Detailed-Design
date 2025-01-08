b3f56202234bb8ccb2f6d4082794534a4d4fba2f
AttilaBehan <57575590+AttilaBehan@users.noreply.github.com>
2025-01-08 13:52:28 +0100

    Update Rib Spacing.py

--- Rib Spacing.py
+++ Rib Spacing.py
@@ -1,17 +1,16 @@
-from columnBuckling import colSafetyMargin, criticalColBuckling, stringerStress
-
+from columnBuckling import colSafetyMargin
 safety_margin=5
 span_loc=0 
 ribs=[0]
 
-while span_loc<10.8:
-    while safety_margin>1:
+while span_loc<10.9:
+    while safety_margin>1.1:
         span_loc+=0.01
         safety_margin=colSafetyMargin(ribs[-1],span_loc)
-        if round(span_loc,3) in [3.8,2.84,10.91] or span_loc>10.9:
+        if round(span_loc,3) in [3.80,2.84,10.91] or span_loc>10.91:
             break
     ribs+=[round(span_loc,3)]
-    safety_margin=1.1
+    safety_margin=5
     
 print(ribs)
 print(len(ribs))
